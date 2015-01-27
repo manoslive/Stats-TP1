5bd30cf5a5f756064f67ccabfdda54fb89e69ffc
manoslive <ebeloin@hotmail.com>
2015-01-27 17:47:46 -0500

    Version non fonctionnelle

--- Form1.cs
+++ Form1.cs
@@ -13,13 +13,9 @@ namespace tp1_echantillonnage
 {
     public partial class Form1 : Form
     {
-        public int TotalRowCount;
-        Excel.Application xlApp = new Excel.Application();
-        Excel.Workbook xlWorkBook;        
-        
         public Form1()
         {
-            InitializeComponent(); 
+            InitializeComponent();
         }
 
         private void BTN_ChoisirFichier_Click(object sender, EventArgs e)
@@ -42,85 +38,14 @@ namespace tp1_echantillonnage
             }
         }
 
-        private List<int> ModeAleatoireSimple()
-        {
-            Random random = new Random();
-            var tableauRangees = new List<int> { };
-            // Remplit la liste des éléments
-            for (int i = 0; i < TotalRowCount; i++)
-            {
-                tableauRangees.Add(i);
-            }
-            for (int i = 0; i < longueur; i++)
-            {
-                for (int j = 0; j < Convert.ToInt32(TB_TailleEchantillons.Text); j++)
-                {
-                    int index = random.Next(tableauRangees.Count);
-                    var rangee = tableauRangees[index];
-                    tableauRangees.RemoveAt(index);
-                    // écrire dans XL
-                    //return tableauRangees;
-                }
-
-            }
-
-
-        }
-
-
-        private void BTN_Generer_Click(object sender, EventArgs e)
-        {
-            DGV_Fichier.Rows.Clear();
-            RemplirDGVFichier();
-            if (RB_AleatoireSimple.Checked == true)
-            {
-                ModeAleatoireSimple();
-            }
-            else if (RB_Systematique.Checked == true)
-            {
-                // ModeSystematique();
-            }
-            BTN_Save.Enabled = true;
-        }
-
-        private void RemplirDGVFichier()
-        {
-            string nomsFichiers = TB_NomsFichiers.Text;
-            int nbEchantillons = Convert.ToInt32(TB_NbEchantillons.Text);
-            int tailleEchantillons = Convert.ToInt32(TB_TailleEchantillons.Text);
-
-            for (int i = 0; i < nbEchantillons; i++)
-            {
-                DGV_Fichier.Rows.Add(nomsFichiers + " " + (i + 1));
-            }
-
-            // Resize les cells du DGV
-            DGV_Fichier.AutoResizeColumnHeadersHeight();
-            DGV_Fichier.AutoResizeRows(DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders);
-        }
-
-        private void BTN_Save_Click(object sender, EventArgs e)
-        {
-            SaveFiles();
-        }
-
-        private void RemplirWorksheet(Excel.Worksheet xlWorkSheet)
-        {
-            for (int i = 0; i <= DGV_Echantillon.RowCount - 1; i++)
-            {
-                for (int j = 0; j <= DGV_Echantillon.ColumnCount - 1; j++)
-                {
-                    xlWorkSheet.Cells[i + 1, j + 1] = DGV_Echantillon[j, i].Value;
-                }
-            }
-        }
-
         private void SaveFiles()
         {
             FolderBrowserDialog ChoisirPath = new FolderBrowserDialog();
 
             if (ChoisirPath.ShowDialog() == DialogResult.OK)
             {
+                Excel.Application xlApp = new Excel.Application();
+                Excel.Workbook xlWorkBook;
                 Excel.Worksheet xlWorkSheet;
                 object misValue = System.Reflection.Missing.Value;
 
@@ -129,12 +54,59 @@ namespace tp1_echantillonnage
                     xlApp = new Excel.Application();
                     xlWorkBook = xlApp.Workbooks.Add(misValue);
                     xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
-
-                    RemplirWorksheet(xlWorkSheet);
+                    ModeAleatoireSimple(xlWorkSheet);
+                    //RemplirWorksheet(xlWorkSheet);
 
                     xlWorkBook.SaveAs(ChoisirPath.SelectedPath + "\\" + TB_NomsFichiers + x, Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
                     xlWorkBook.Close(true, misValue, misValue);
                     xlApp.Quit();
+                }
+            }
+        }
+
+        private void BTN_Save_Click(object sender, EventArgs e)
+        {
+            SaveFiles();
+        }
+
+        private void ModeAleatoireSimple(Excel.Worksheet worksheet)
+        {
+            Random random = new Random();
+            var tableauRangees = new List<int> { };
+            var tableauEchantillon = new List<int> { };
+            // Remplit la liste des éléments
+            for (int i = 0; i < worksheet.UsedRange.Rows.Count; i++)
+            {
+                tableauRangees.Add(i);
+            }
+            for (int i = 0; i < Convert.ToInt32(TB_NbEchantillons.Text); i++)
+            {
+                for (int j = 0; j < Convert.ToInt32(TB_TailleEchantillons.Text); j++)
+                {
+                    int index = random.Next(tableauRangees.Count);
+                    var rangee = tableauRangees[index];
+                    tableauEchantillon.Add(rangee);
+                    tableauRangees.RemoveAt(index);
+                    // écrire dans XL
+                    for (int k = 0; k <= // Nombre de rangees - 1; k++)
+                    {
+
+                        Excel.Application xlAppli1 = new Excel.Application();
+                        Excel.Workbook xlWorkBook1;
+                        Excel.Worksheet xlEchantillon;
+                        object misValue = System.Reflection.Missing.Value;
+                        xlWorkBook1 = xlAppli1.Workbooks.Add(misValue);
+                        xlEchantillon = (Excel.Worksheet)xlWorkBook1.Worksheets.get_Item(1);
+
+
+                        for (int l = 0; l <= //Nombre de colonnes - 1; l++)
+                        {
+                            int rangeeWS = tableauEchantillon[k];
+                            xlEchantillon.Cells[k + 1, l + 1] = worksheet.Cells[rangeeWS, l + 1];
+                        }
+                        xlWorkBook1.Close(true, misValue, misValue);
+                        xlAppli1.Quit();
+                    }
                 }
             }
         }
